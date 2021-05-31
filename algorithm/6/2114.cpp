7da593a11de2a9a42c8ad126c850d74ad8f2f867
sztangcy <your email>
2021-05-31 08:47:43 +0800

    add 7 2412 and 8.md

--- algorithm/6/2114.cpp
+++ algorithm/6/2114.cpp
@@ -1,19 +1,49 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int n;
-int map[100010];
+
+//  问题 ：无法输出，就根本没输出。
+//1. cout 输出语句没写
+//2. 在满足什么条件下输出，这个条件不对。去看条件，和题目逻辑对应。
+//
+//建议一点点写。
+//
+int n_occur[100005] = { 0 };
+
 int main() {
-  cin >> n;
-  memset(map, 0, sizeof(map));
-  int a;
-  for (int i = 1; i <= n; i ++) {
+  int n, m;
+
+  cin >> n >> m;
+  for (int i = 1; i <= m; i++) {
+    int a;
     cin >> a;
-    map[a] ++;
+    n_occur[a] = n_occur[a] + 1;
+
   }
-  for (int i = 1; i <= n; i ++) {
-    if (map[i] > 1) {
-      cout << i << " " << map[i] << endl;
-    }
+
+  for (int i = 1; i <= n; i++) {
+    if (n_occur[i] > 1)
+      cout << i << " " << n_occur[i] << endl;
   }
-  return 0;
 }
+
+
+
+////#include<bits/stdc++.h>
+//using namespace std;
+//int n;
+//int map[100010];
+//int main() {
+//  cin >> n;
+//  memset(map, 0, sizeof(map));
+//  int a;
+//  for (int i = 1; i <= n; i ++) {
+//    cin >> a;
+//    map[a] ++;
+//  }
+//  for (int i = 1; i <= n; i ++) {
+//    if (map[i] > 1) {
+//      cout << i << " " << map[i] << endl;
+//    }
+//  }
+//  return 0;
+//}
---
+++ algorithm/7/2412.cpp
@@ -0,0 +1,13 @@
+//
+// Created by YMXD on 2021/5/31.
+//
+//1. 最简单做法，10分钟内
+//2.
+
+for(i) {
+
+  for (j)
+{cout << j << "*" << i << "=" << i * j << endl;}
+  cout << endl;
+}
+
